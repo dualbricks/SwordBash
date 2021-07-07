30a47767e685fe3c543a1fb34b15bd296073566e
dualbricks <wenyuemawen@gmail.com>
2021-07-08 02:45:18 +0800

    movement added still buggy

--- main.py
+++ main.py
@@ -15,6 +15,13 @@ ground_value = 400
 ground = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background", "tile000.png")), (100, 100))
 
 
+# character
+main_player = player.Player()
+main_player.x = 0
+main_player.y = ground_value - 32
+speed = 5
+
+
 def bg_music():
     pygame.mixer.music.load(os.path.join("assets", "music", "8bit Stage3 Loop.wav"))
     pygame.mixer.music.play(-1, 0.0)
@@ -25,14 +32,34 @@ def ground_floor():
         WIN.blit(ground, (x, ground_value))
 
 
+def actions(key_pressed):
+    if key_pressed[pygame.K_RIGHT]:
+        main_player.update_running_right()
+        WIN.blit(main_player.runRight[main_player.runRightIndex // 6], (main_player.x + speed, main_player.y))
+        main_player.x += speed
+    elif key_pressed[pygame.K_SPACE]:
+        main_player.update_jumping()
+        WIN.blit(main_player.jump[main_player.jumpIndex // 6], (main_player.x, main_player.y - speed*2))
+    elif key_pressed[pygame.K_LEFT]:
+        main_player.update_running_left()
+        WIN.blit(main_player.runLeft[main_player.runLeftIndex // 6], (main_player.x - speed, main_player.y))
+        main_player.x -= speed
+    else:
+        main_player.update_idle()
+        WIN.blit(main_player.idle[main_player.idleIndex // 6], (main_player.x, main_player.y))
+
+
 def main():
     clock = pygame.time.Clock()
     bg_music()
     run = True
     while run:
+        clock.tick(FPS)
+        WIN.blit(background, (0, 0))
+        ground_floor()
         for event in pygame.event.get():
-            WIN.blit(background, (0, 0))
-            ground_floor()
+            key_pressed = pygame.key.get_pressed()
+            actions(key_pressed)
             pygame.display.update()
             if event.type == pygame.QUIT:
                 run = False
--- player.py
+++ player.py
@@ -1,4 +1,4 @@
-import pygame.sprite
+import pygame
 import os
 
 
@@ -14,20 +14,23 @@ class Player():
 
         self.idleIndex = 0
 
-        self.idling = self.idle[self.idleIndex]
+        self.runRight = [pygame.image.load(os.path.join("assets", "animations", "running", "tile000.png")),
+                         pygame.image.load(os.path.join("assets", "animations", "running", "tile001.png")),
+                         pygame.image.load(os.path.join("assets", "animations", "running", "tile002.png")),
+                         pygame.image.load(os.path.join("assets", "animations", "running", "tile003.png")),
+                         pygame.image.load(os.path.join("assets", "animations", "running", "tile004.png")),
+                         pygame.image.load(os.path.join("assets", "animations", "running", "tile005.png")),
+                         pygame.image.load(os.path.join("assets", "animations", "running", "tile006.png")),
+                         pygame.image.load(os.path.join("assets", "animations", "running", "tile007.png"))]
 
-        self.run = [pygame.image.load(os.path.join("assets", "animations", "running", "tile000.png")),
-                    pygame.image.load(os.path.join("assets", "animations", "running", "tile001.png")),
-                    pygame.image.load(os.path.join("assets", "animations", "running", "tile002.png")),
-                    pygame.image.load(os.path.join("assets", "animations", "running", "tile003.png")),
-                    pygame.image.load(os.path.join("assets", "animations", "running", "tile004.png")),
-                    pygame.image.load(os.path.join("assets", "animations", "running", "tile005.png")),
-                    pygame.image.load(os.path.join("assets", "animations", "running", "tile006.png")),
-                    pygame.image.load(os.path.join("assets", "animations", "running", "tile007.png"))]
+        self.runRightIndex = 0
 
-        self.runIndex = 0
+        self.runLeft = []
 
-        self.running = self.run[self.idleIndex]
+        for frames in self.runRight:
+            self.runLeft.append(pygame.transform.flip(frames, True, False))
+
+        self.runLeftIndex = 0
 
         self.jump = [pygame.image.load(os.path.join("assets", "animations", "jump", "tile000.png")),
                      pygame.image.load(os.path.join("assets", "animations", "jump", "tile001.png")),
@@ -36,31 +39,32 @@ class Player():
 
         self.jumpIndex = 0
 
-        self.jumping = self.jump[self.jumpIndex]
-
     def update_idle(self):
 
         self.idleIndex += 1
 
-        if self.idleIndex >= len(self.idle):
+        if self.idleIndex >= len(self.idle) * 5:
             self.idleIndex = 0
 
-        self.idling = self.idle[self.idleIndex]
+    def update_running_right(self):
 
-    def update_running(self):
+        self.runRightIndex += 1
 
-        self.runIndex += 1
+        if self.runRightIndex >= len(self.runRight) * 6:
 
-        if self.runIndex >= len(self.run):
-            self.runIndex = 0
-
-        self.running = self.run[self.runIndex]
+            self.runRightIndex = 0
 
     def update_jumping(self):
 
         self.jumpIndex += 1
 
-        if self.jumpIndex >= len(self.jump):
+        if self.jumpIndex >= len(self.jump) * 5:
             self.jumpIndex = 0
 
-        self.jumping = self.jump[self.jumpIndex]
+    def update_running_left(self):
+
+        self.runLeftIndex += 1
+
+        if self.runLeftIndex >= len(self.runLeft) * 6:
+
+            self.runLeftIndex = 0
